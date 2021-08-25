9f464157c0eae90751d6f8c86b2cab5d0cad7842
Sami Sayegh <ssayegh@coveo.com>
2021-08-25 11:59:23 -0400

    add ResultList

---
+++ src/components/ResultList.tsx
@@ -0,0 +1,109 @@
+import * as React from 'react';
+import List from '@material-ui/core/List';
+import {ListItem, Box, Typography, ListItemProps} from '@material-ui/core';
+import {
+  buildResultList,
+  Result,
+  buildResultTemplatesManager,
+  ResultTemplatesManager,
+  ResultList as HeadlessResultList,
+} from '@coveo/headless';
+import {EngineContext} from '../context/engine';
+
+type Template = (result: Result) => React.ReactNode;
+
+interface FieldValueInterface {
+  value: string;
+  caption: string;
+}
+
+interface ResultListProps {
+  controller: HeadlessResultList;
+}
+function ListItemLink(props: ListItemProps<'a'>) {
+  return (
+    <ListItem {...props} button component="a">
+      <Typography variant="body1" color="primary">
+        {props.title}
+      </Typography>
+    </ListItem>
+  );
+}
+
+function FieldValue(props: FieldValueInterface) {
+  return (
+    <Box>
+      <Typography
+        color="textSecondary"
+        style={{fontWeight: 'bold'}}
+        variant="caption"
+      >
+        {props.caption}:&nbsp;
+      </Typography>
+      <Typography color="textSecondary" variant="caption">
+        {props.value}
+      </Typography>
+    </Box>
+  );
+}
+
+const ResultListRenderer: React.FunctionComponent<ResultListProps> = (props) => {
+  const {controller} = props;
+  const engine = React.useContext(EngineContext)!;
+  const [state, setState] = React.useState(controller.state);
+
+  const headlessResultTemplateManager: ResultTemplatesManager<Template> =
+    buildResultTemplatesManager(engine);
+
+  headlessResultTemplateManager.registerTemplates({
+    conditions: [],
+    content: (result: Result) => (
+      <ListItem disableGutters key={result.uniqueId}>
+        <Box my={2}>
+          <Box pb={1}>
+            <ListItemLink
+              disableGutters
+              title={result.title}
+              href={result.clickUri}
+            />
+          </Box>
+
+          {result.excerpt && (
+            <Box pb={1}>
+              <Typography color="textPrimary" variant="body2">
+                {result.excerpt}
+              </Typography>
+            </Box>
+          )}
+
+          {result.raw.source && (
+            <FieldValue caption="Source" value={result.raw.source} />
+          )}
+          {result.raw.objecttype && (
+            <FieldValue caption="Object Type" value={result.raw.objecttype} />
+          )}
+        </Box>
+      </ListItem>
+    ),
+  });
+
+  React.useEffect(
+    () => controller.subscribe(() => setState(controller.state)),
+    [controller]
+  );
+
+  return (
+    <List>
+      {state.results.map((result: Result) => {
+        const template = headlessResultTemplateManager.selectTemplate(result);
+        return template ? template(result) : null;
+      })}
+    </List>
+  );
+};
+
+export const ResultList = () => {
+  const engine = React.useContext(EngineContext)!;
+  const controller = buildResultList(engine);
+  return <ResultListRenderer controller={controller} />;
+};
--- src/pages/SearchPage.tsx
+++ src/pages/SearchPage.tsx
@@ -2,13 +2,19 @@ import * as React from 'react';
 import { buildSearchEngine, getSampleSearchEngineConfiguration } from "@coveo/headless"
 import {SearchBox} from '../components/SearchBox';
 import {EngineContext} from '../context/engine';
+import {ResultList} from '../components/ResultList';
 
 export function SearchPage() {
   const engine = buildSearchEngine({configuration: getSampleSearchEngineConfiguration()});
 
+  React.useEffect(() => {
+    engine.executeFirstSearch();
+  }, [engine]);
+
   return <div>
     <EngineContext.Provider value={engine}>
-      <SearchBox></SearchBox>
+      <SearchBox/>
+      <ResultList/>
     </EngineContext.Provider>
   </div>
 }
